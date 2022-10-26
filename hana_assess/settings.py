8913c92111536408376fa243c33f5070fcf728a7
Takematsu <kaientai100@gmail.com>
2022-10-26 16:48:39 +0900

    デプロイ前その３

--- hana_assess/settings.py
+++ hana_assess/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5+z08gw_%yvm(0o4hmffg(xyn^_$$j&c5#hovffnu3wt8zkp(8'
+SECRET_KEY = []
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
