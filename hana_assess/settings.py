17cdd66f18655a49239370956448ac5ace0deea8
Takematsu <kaientai100@gmail.com>
2022-10-27 11:48:00 +0900

    デプロイ前の準備:wq

--- hana_assess/settings.py
+++ hana_assess/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4*(gkh59u3v$ab8l33nhcb0kj#1n_)n$u)8$kxtuxhg-1pn6o8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
